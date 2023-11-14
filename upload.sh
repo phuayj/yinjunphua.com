8dc1bf82bae7ff9bacdcf3bb32f6bfdc666f8fc4
Yin Jun Phua <phua@comp.isct.ac.jp>
2023-11-14 16:21:40 +0900

    Initial commit.

---
+++ upload.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PYTHON_VERSION=3.9.7
+eval "$(pyenv init -)"
+aws s3 sync --exclude '.DS_Store' --exclude '*/.DS_Store' --exclude 'upload.sh' --exclude '.gitignore' . s3://yinjunphua.com
