eadb6e0eef59ec24563ade23091d4a2dc9e06840
Yin Jun Phua <phua@comp.isct.ac.jp>
2023-11-17 18:06:30 +0900

    Fix typo and avoid uploading .git file.

--- upload.sh
+++ upload.sh
@@ -2,4 +2,4 @@
 
 export PYTHON_VERSION=3.9.7
 eval "$(pyenv init -)"
-aws s3 sync --exclude '.DS_Store' --exclude '*/.DS_Store' --exclude 'upload.sh' --exclude '.gitignore' . s3://yinjunphua.com
+aws s3 sync --exclude '.DS_Store' --exclude '*/.DS_Store' --exclude 'upload.sh' --exclude '.gitignore' --exclude '.git/*' . s3://yinjunphua.com
