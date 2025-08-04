0118d5848eb47f51aa81574d2b10d2a87c4d047f
Yin Jun Phua <phua@comp.isct.ac.jp>
2025-08-04 16:21:18 +0900

    Various updates.

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PYTHON_VERSION=3.9.7
-eval "$(pyenv init -)"
-aws s3 sync --exclude '.DS_Store' --exclude '*/.DS_Store' --exclude 'upload.sh' --exclude '.gitignore' --exclude '.git/*' . s3://yinjunphua.com
+. ./venv/bin/activate
+
+aws s3 sync --exclude 'venv/*' --exclude '.DS_Store' --exclude '*/.DS_Store' --exclude 'upload.sh' --exclude '.gitignore' --exclude '.git/*' . s3://yinjunphua.com
