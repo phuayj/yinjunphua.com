3869043672b82a52298ce776440b5c21e3fa87e5
Yin Jun Phua <phua@comp.isct.ac.jp>
2025-08-28 13:10:24 +0900

    Add invited talks and github action.

--- upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-. ./venv/bin/activate
-
-aws s3 sync --exclude 'venv/*' --exclude '.DS_Store' --exclude '*/.DS_Store' --exclude 'upload.sh' --exclude '.gitignore' --exclude '.git/*' . s3://yinjunphua.com
