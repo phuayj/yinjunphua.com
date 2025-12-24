50838dc9c8437d31d54daa454db3edb48ae089ea
Yin Jun Phua <phua@comp.isct.ac.jp>
2025-12-24 13:56:27 +0900

    Making things easier to organize.

---
+++ src/_data/publications.js
@@ -0,0 +1,94 @@
+module.exports = {
+  en: [
+    {
+      title: "Variable Assignment Invariant Neural Networks for Learning Logic Programs",
+      url: "https://arxiv.org/abs/2408.10709",
+      authors: "<strong>Yin Jun Phua</strong>, Katsumi Inoue",
+      venue: "18th International Conference on Neural-Symbolic Learning and Reasoning, 2024.",
+    },
+    {
+      title: "Class-Incremental Learning using Diffusion Model for Distillation and Replay",
+      url: "https://openaccess.thecvf.com/content/ICCV2023W/VCL/html/Jodelet_Class-Incremental_Learning_Using_Diffusion_Model_for_Distillation_and_Replay_ICCVW_2023_paper.html",
+      note: "[Best Paper Award]",
+      authors: "Quentin Jodelet, Xin Liu, <strong>Yin Jun Phua</strong>, Tsuyoshi Murata",
+      venue: "1st Workshop on Visual Continual Learning, ICCV2023, 2023.",
+    },
+    {
+      title: "resVAE ensemble: Unsupervised identification of gene sets in multi-modal single-cell sequencing data using deep ensembles",
+      url: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9975353/",
+      authors: "Foo Wei Ten, Dong Sheng Yuan, Nabil Jabareen, <strong>Yin Jun Phua</strong>, Richard Eils, Soeren Lukassen, Christian Conrad",
+      venue: "Frontiers in Cell and Developmental Biology 11, 2023.",
+    },
+    {
+      title: "Learning Logic Programs Using Neural Networks by Exploiting Symbolic Invariance",
+      url: "https://dblp.org/rec/conf/ilp/PhuaI21",
+      authors: "<strong>Yin Jun Phua</strong>, Katsumi Inoue",
+      venue: "ILP, 203-218, 2021.",
+    },
+    {
+      title: "Learning Logic Programs from Noisy State Transition Data",
+      url: "https://dblp.org/rec/conf/ilp/PhuaI19",
+      note: "[Best Student Paper Award]",
+      authors: "<strong>Yin Jun Phua</strong>, Katsumi Inoue",
+      venue: "Inductive Logic Programming - 29th International Conference(ILP), JSAI2019 72-80, 2019.",
+    },
+    {
+      title: "Learning Representation of Relational Dynamics with Delays and Refining with Prior Knowledge",
+      url: "https://dblp.org/rec/journals/flap/PhuaRI19",
+      authors: "<strong>Yin Jun Phua</strong>, Tony Ribeiro, Katsumi Inoue",
+      venue: "IfCoLog Journal of Logics and their Applications(FLAP), 6(4) 695-708, 2019.",
+    },
+    {
+      title: "Learning Logic Program Representation for Delayed Systems With Limited Training Data",
+      url: "https://dblp.org/rec/conf/ilp/PhuaI19",
+      authors: "<strong>Yin Jun Phua</strong>, Tony Ribeiro, Sophie Tourret, Katsumi Inoue",
+      venue: "Late Breaking Papers of the 27th International Conference on Inductive Logic Programming, 27-37, 2017.",
+    },
+  ],
+  ja: [
+    {
+      title: "Variable Assignment Invariant Neural Networks for Learning Logic Programs",
+      url: "https://arxiv.org/abs/2408.10709",
+      authors: "<strong>Yin Jun Phua</strong>, Katsumi Inoue",
+      venue: "18th International Conference on Neural-Symbolic Learning and Reasoning, 2024.",
+    },
+    {
+      title: "Class-Incremental Learning using Diffusion Model for Distillation and Replay",
+      url: "https://openaccess.thecvf.com/content/ICCV2023W/VCL/html/Jodelet_Class-Incremental_Learning_Using_Diffusion_Model_for_Distillation_and_Replay_ICCVW_2023_paper.html",
+      note: "[最優秀論文賞]",
+      authors: "Quentin Jodelet, Xin Liu, <strong>Yin Jun Phua</strong>, Tsuyoshi Murata",
+      venue: "1st Workshop on Visual Continual Learning, ICCV2023, 2023.",
+    },
+    {
+      title: "resVAE ensemble: Unsupervised identification of gene sets in multi-modal single-cell sequencing data using deep ensembles",
+      url: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9975353/",
+      authors: "Foo Wei Ten, Dong Sheng Yuan, Nabil Jabareen, <strong>Yin Jun Phua</strong>, Richard Eils, Soeren Lukassen, Christian Conrad",
+      venue: "Frontiers in Cell and Developmental Biology 11, 2023.",
+    },
+    {
+      title: "Learning Logic Programs Using Neural Networks by Exploiting Symbolic Invariance",
+      url: "https://dblp.org/rec/conf/ilp/PhuaI21",
+      authors: "<strong>Yin Jun Phua</strong>, Katsumi Inoue",
+      venue: "ILP, 203-218, 2021.",
+    },
+    {
+      title: "Learning Logic Programs from Noisy State Transition Data",
+      url: "https://dblp.org/rec/conf/ilp/PhuaI19",
+      note: "[最優秀学生論文賞]",
+      authors: "<strong>Yin Jun Phua</strong>, Katsumi Inoue",
+      venue: "Inductive Logic Programming - 29th International Conference(ILP), JSAI2019 72-80, 2019.",
+    },
+    {
+      title: "Learning Representation of Relational Dynamics with Delays and Refining with Prior Knowledge",
+      url: "https://dblp.org/rec/journals/flap/PhuaRI19",
+      authors: "<strong>Yin Jun Phua</strong>, Tony Ribeiro, Katsumi Inoue",
+      venue: "IfCoLog Journal of Logics and their Applications(FLAP), 6(4) 695-708, 2019.",
+    },
+    {
+      title: "Learning Logic Program Representation for Delayed Systems With Limited Training Data",
+      url: "https://dblp.org/rec/conf/ilp/PhuaI19",
+      authors: "<strong>Yin Jun Phua</strong>, Tony Ribeiro, Sophie Tourret, Katsumi Inoue",
+      venue: "Late Breaking Papers of the 27th International Conference on Inductive Logic Programming, 27-37, 2017.",
+    },
+  ],
+};
